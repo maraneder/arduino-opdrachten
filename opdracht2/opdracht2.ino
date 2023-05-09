b9b3b5a4859273864cabe7704540e7c12044cda4
Mara <mm.nederlof@ad-academie.nl>
2023-05-09 11:02:09 +0200

    opdracht 2

---
+++ opdracht2/opdracht2.ino
@@ -0,0 +1,67 @@
+#define DELAY 50
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(DELAY);
+  digitalWrite(12, HIGH);
+  delay(DELAY);
+  digitalWrite(13, LOW);
+  digitalWrite(11, HIGH);
+  delay(DELAY);
+  digitalWrite(12, LOW);
+  digitalWrite(10, HIGH);
+  delay(DELAY);
+  digitalWrite(11, LOW);
+  digitalWrite(9, HIGH);
+  delay(DELAY);
+  digitalWrite(10,LOW);
+  digitalWrite(8, HIGH);
+  delay(DELAY);
+  digitalWrite(9, LOW);
+  digitalWrite(7, HIGH);
+  delay(DELAY);
+  digitalWrite(8, LOW);
+  digitalWrite(6, HIGH);
+  delay(DELAY);
+  digitalWrite(7, LOW);
+  delay(DELAY);
+
+  delay(DELAY);
+  digitalWrite(7, HIGH);
+  delay(DELAY);
+  digitalWrite(8, HIGH);
+  digitalWrite(6, LOW);
+  delay(DELAY);
+  digitalWrite(7, LOW);
+  digitalWrite(9, HIGH);
+  delay(DELAY);
+  digitalWrite(10, HIGH);
+  digitalWrite(8, LOW);
+  delay(DELAY);
+  digitalWrite(11, HIGH);
+  digitalWrite(9, LOW);
+  delay(DELAY);
+  digitalWrite(12, HIGH);
+  digitalWrite(10, LOW);
+  delay(DELAY);
+  digitalWrite(12, LOW);
+  digitalWrite(13, HIGH);
+  digitalWrite(11, LOW);
+  delay(DELAY);
+
+}
+
+
